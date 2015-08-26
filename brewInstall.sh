74a1736aa9c788cf5e005cf38931451fba180f17
neohuang <superneohuang@gmail.com>
2015-08-26 14:58:11 +0200

    add brew install list and some minor config

---
+++ brewInstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+brew install zsh
+brew install tmux
+brew install cmake
+brew install macvim
+brew install go
+brew install entr
+brew install jq
+brew install zsh-completions
+brew install the_silver_searcher
+brew install node
+brew install reattach-to-user-namespace
